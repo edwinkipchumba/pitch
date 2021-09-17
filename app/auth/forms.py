df13145cbf1ca90b1b25652667cf701ea7a55438
edwinkipchumba <edwin.kolem@student.moringaschool.com>
2021-09-17 21:37:12 +0300

    validate login credentials

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -17,3 +17,10 @@ class RegistrationForm(FlaskForm):
     def validate_author(self,data_field):
         if User.query.filter_by(author = data_field.data).first():
             raise ValidationError('That author name is taken')
+
+        # login section
+class LoginForm(FlaskForm):
+    email = StringField('Your Email Address',validators=[Required(),Email()])
+    password = PasswordField('Password',validators =[Required()])
+    remember = BooleanField('Remember me')
+    submit = SubmitField('Sign In')
