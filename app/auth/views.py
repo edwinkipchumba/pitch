6b8a139d8395d73a3cfb2cf338618d40317f1076
edwinkipchumba <edwin.kolem@student.moringaschool.com>
2021-09-17 21:46:37 +0300

    adding logout route

--- app/auth/views.py
+++ app/auth/views.py
@@ -41,3 +41,11 @@ def register():
         return redirect(url_for('auth.login'))
 
     return render_template('auth/register.html',registration_form = form)
+
+    # route logout
+    
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for("main.index"))
