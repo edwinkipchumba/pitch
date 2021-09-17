3367677375680103fee2140ad2def0af0c59957e
edwinkipchumba <edwin.kolem@student.moringaschool.com>
2021-09-17 21:08:51 +0300

    add class pictch model

--- app/models.py
+++ app/models.py
@@ -33,3 +33,17 @@ class User(UserMixin,db.Model):
 
     def __repr__(self):
         return f'Author: {self.author}'
+
+class Pitches(db.Model):
+    __tablename__= 'pitches'
+    id = db.Column(db.Integer,primary_key = True)
+    title = db.Column(db.String(255))
+    category = db.Column(db.String(255))
+    pitch = db.Column(db.String(255))
+    date = db.Column(db.DateTime(250), default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+    comments = db.relationship('Comments', backref='title', lazy='dynamic')
+
+    def save_pitch(self):
+        db.session.add(self)
+        db.session.commit()
