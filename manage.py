7e36745b24e17ece208b5a9fc2e285ef4e04e0ae
edwinkipchumba <edwin.kolem@student.moringaschool.com>
2021-09-17 20:37:59 +0300

    adding function to run test

--- manage.py
+++ manage.py
@@ -4,3 +4,14 @@ from  flask_migrate import Migrate, MigrateCommand
 from app.models import User, Pitches, Comments
 
 app = create_app('development')
+
+manager = Manager(app)
+manager.add_command('server',Server)
+
+
+@manager.command
+def test():
+    """Run the unit tests."""
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
