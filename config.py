98e07599f7c12a32075e2aed8e1840478c2bb914
edwinkipchumba <edwin.kolem@student.moringaschool.com>
2021-09-17 20:51:20 +0300

    adding production config child class

--- config.py
+++ config.py
@@ -15,8 +15,20 @@ class Config:
     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
     SUBJECT_PREFIX = 'Pitch It Up!'
-    SENDER_EMAIL = 'staremdee@gmail.com'
+    SENDER_EMAIL = 'edwinkolem@gmail.com'
 
     @staticmethod
     def init_app(app):
         pass
+
+
+class ProdConfig(Config):
+    '''
+    Production  configuration child class
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+
+class TestConfig(Config):
+    SQLALCHEMY_DATABASE_URI ='postgresql+psycopg2://edu:kmox002@localhost/pitches_test'
