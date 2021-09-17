08045591803c04b8be137fb4b96994fdd6f36a1d
edwinkipchumba <edwin.kolem@student.moringaschool.com>
2021-09-17 20:58:45 +0300

    adding commentmodel test

--- tests/test_user.py
+++ tests/test_user.py
@@ -15,3 +15,20 @@ class UserModelTest(unittest.TestCase):
 
     def test_password_verification(self):
         self.assertTrue(self.new_user.verify_password('banana'))
+
+class CommentsModelTest(unittest.TestCase):
+    
+    def setUp(self):
+       self.new_comment = Comments(id=1, user_id = 2, comment = 'cross buns',pitches_id = '5',date_posted='2018-09-09')
+
+    def test_comment_variables(self):
+
+       # self.assertEquals(self.new_comment.id, 2)
+       self.assertEquals(self.new_comment.comment,'cross buns')
+       self.assertEquals(self.new_comment.date_posted,'2018-09-09')
+       self.assertEquals(self.new_comment.user_id, 2)
+       # self.assertEquals(self.new_comment.pitches_id, 4)
+
+    def test_save_comment(self):
+
+        self.assertTrue(len(Comments.query.all())>0)
