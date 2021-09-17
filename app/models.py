1baa26d78bcd1d282b4e6483240691fb3e856ca9
edwinkipchumba <edwin.kolem@student.moringaschool.com>
2021-09-17 21:15:34 +0300

    add save and get comment

--- app/models.py
+++ app/models.py
@@ -47,3 +47,31 @@ class Pitches(db.Model):
     def save_pitch(self):
         db.session.add(self)
         db.session.commit()
+
+    @classmethod
+    def get_pitches(cls,cate):
+        pitch = Pitches.query.filter_by(category=cate).all()
+        return pitch
+
+    def __repr__(self):
+        return f"Pitches {self.pitch}','{self.date}')"
+
+class Comments(db.Model):
+    __tablename__ = 'comments'
+    id = db.Column(db.Integer, primary_key=True)
+    comment = db.Column(db.String(255))
+    date_posted = db.Column(db.DateTime(250), default=datetime.utcnow)
+    pitches_id = db.Column(db.Integer, db.ForeignKey("pitches.id"))
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+
+    def save_comment(self):
+        db.session.add(self)
+        db.session.commit()
+
+    @classmethod
+    def get_comment(cls,id):
+        comments = Comments.query.filter_by(pitches_id=id).all()
+        return comments
+
+    def __repr__(self):
+        return f"Comments('{self.comment}', '{self.date_posted}')"
