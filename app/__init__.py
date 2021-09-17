85ef58b11f65a64efc20d688d09ebabf1a0fae23
edwinkipchumba <edwin.kolem@student.moringaschool.com>
2021-09-17 21:24:49 +0300

    add initializing flask

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,25 @@
 from flask import Flask
+from flask_bootstrap import Bootstrap
+from config import config_options
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_uploads import UploadSet,configure_uploads,IMAGES
+from flask_mail import Mail
 
-# Initializing application
-app = Flask(__name__)
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
 
-from app import views
+bootstrap = Bootstrap()
+db = SQLAlchemy()
+photos = UploadSet('photos',IMAGES)
+mail = Mail()
+
+def create_app(config_name):
+    
+
+    app = Flask(__name__)
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint,url_prefix = '/authenticate')
