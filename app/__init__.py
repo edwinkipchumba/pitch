eccb0973985fe59770f3dd1c367cd47c34affc18
edwinkipchumba <edwin.kolem@student.moringaschool.com>
2021-09-18 17:45:23 +0300

    adding specs markdown

--- app/__init__.py
+++ app/__init__.py
@@ -39,6 +39,9 @@ def create_app(config_name):
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
+    # secret
+    app.secret_key='SECRET_KEY'
+
 
 
     return app
