59d94df6aedc62b874eff1a1202a39501f98177c
edwinkipchumba <edwin.kolem@student.moringaschool.com>
2021-09-18 11:38:44 +0300

    install flask-simplemde

--- config.py
+++ config.py
@@ -5,22 +5,10 @@ class Config:
     SECRET_KEY=os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI ='postgresql+psycopg2://edu:kmox002localhost/pitches'
 
-
+    #  image uploader
     UPLOADED_PHOTOS_DEST ='app/static/photos'
-
-       #  email configurations
-    MAIL_SERVER = 'smtp.googlemail.com'
-    MAIL_PORT = 587
-    MAIL_USE_TLS = True
-    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
-    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
-    SUBJECT_PREFIX = 'Pitch It Up!'
-    SENDER_EMAIL = 'edwinkolem5@gmail.com'
-
-    @staticmethod
-    def init_app(app):
-        pass
-
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+  
 
 class ProdConfig(Config):
     '''
@@ -40,9 +28,32 @@ class DevConfig(Config):
     Args:
         Config: The parent configuration class with General configuration settings
     '''
+    # connecting to database
     SQLALCHEMY_DATABASE_URI ='postgresql+psycopg2://edu:kmox002@localhost/pitches'
 
     DEBUG = True
+
+         # connecting to Gmail
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    SUBJECT_PREFIX = 'Pitch It Up!'
+    SENDER_EMAIL = 'edwinkolem5@gmail.com'
+
+  # editor
+class Config:
+    # simple mde  configurations
+    SIMPLEMDE_JS_IIFE = True
+    SIMPLEMDE_USE_CDN = True
+
+
+    @staticmethod
+    def init_app(app):
+        pass
+
+  
 
 config_options = {
 'development':DevConfig,
