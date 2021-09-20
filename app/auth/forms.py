5b8a17335c0d59ae575f7825f68499a109ed470f
edwinkipchumba <edwin.kolem@student.moringaschool.com>
2021-09-20 12:16:14 +0300

    make it better

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -9,7 +9,7 @@ class LoginForm(FlaskForm):
     email = StringField('Your Email Address',validators=[Required(),Email()])
     password = PasswordField('Password',validators =[Required()])
     remember = BooleanField('Remember me')
-    submit = SubmitField('Sign In')
+    submit = SubmitField('Login')
 
 # registration form
 class RegistrationForm(FlaskForm):
