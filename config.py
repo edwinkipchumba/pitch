cf012e8d60838ce4c6d7ea81a428bd9f6bf1fe91
edwinkipchumba <edwin.kolem@student.moringaschool.com>
2021-09-21 14:58:04 +0300

    git commit --amend --no-edit

--- config.py
+++ config.py
@@ -9,6 +9,19 @@ class Config:
     UPLOADED_PHOTOS_DEST ='app/static/photos'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
   
+        # connecting to Gmail
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    SUBJECT_PREFIX = 'PickUp!'
+    SENDER_EMAIL = 'edwinkolem5@gmail.com'
+
+      # simple mde  configurations
+    SIMPLEMDE_JS_IIFE = True
+    SIMPLEMDE_USE_CDN = True
+
 
 class ProdConfig(Config):
     '''
@@ -29,32 +42,12 @@ class DevConfig(Config):
         Config: The parent configuration class with General configuration settings
     '''
     # connecting to database
-    # SQLALCHEMY_DATABASE_URI ='postgresql+psycopg2://edu:kmox002@localhost/pitches'
+    SQLALCHEMY_DATABASE_URI ='postgresql+psycopg2://edu:kmox002@localhost/pitches'
     
 
     DEBUG = True
 
-         # connecting to Gmail
-    MAIL_SERVER = 'smtp.googlemail.com'
-    MAIL_PORT = 587
-    MAIL_USE_TLS = True
-    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
-    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
-    SUBJECT_PREFIX = 'PickUp!'
-    SENDER_EMAIL = 'edwinkolem5@gmail.com'
 
-  # editor
-class Config:
-    # simple mde  configurations
-    SIMPLEMDE_JS_IIFE = True
-    SIMPLEMDE_USE_CDN = True
-
-
-    @staticmethod
-    def init_app(app):
-        pass
-
-  
 
 config_options = {
 'development':DevConfig,
